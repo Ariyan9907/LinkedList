c8f13c84be6ce6a69f7b9b33242a6931955e454f
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-07-02 11:04:35 +0530

    [Ariyan Pujari] UC3 - Append nodes at end

--- src/linkedlists/service/LinkedList.java
+++ src/linkedlists/service/LinkedList.java
@@ -4,12 +4,33 @@ import linkedlists.model.Node;
 public class LinkedList<T> {
     public Node<T> head;
 
+
     //adding first to node
     public void addFirst(T data){
         Node<T> newNode = new Node<T>(data);
         newNode.next=head;
         head=newNode;
     }
+
+    public void addLast(T data){
+        Node<T> newNode=new Node<T>(data);
+        // If the list is empty
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        // Traverse to the last node
+        Node<T> temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        // Attach the new node
+        temp.next = newNode;
+    }
+
 
     //printing list
     public void display(){
